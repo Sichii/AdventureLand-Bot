e27e2187ae6e57e9acef5e16fda799fc30a2f0b7
Brandon Acton <despereaux@live.com>
2021-03-31 03:57:24 -0400

    fix pi shortcut

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-rm -f "logs/fvr.log"
-forever start -l "/home/pi/Desktop/AdventureLand v3/logs/fvr.log" -o logs/out.log -e logs/err.log build/app.js --color
+forever start -o logs/out.log -e logs/err.log build/app.js --color
